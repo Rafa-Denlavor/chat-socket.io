a28c1864c38bce4db38025c579f2d373b1fd3200
Denlavor <70455070+Rafa-Denlavor@users.noreply.github.com>
2024-02-28 18:05:59 +0000

    fix: two-way communication

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,20 +1,57 @@
-const login = document.querySelector('.login');
-const loginForm = document.querySelector('.login-form');
-const loginInput = document.querySelector('.login-input');
+// --------------- ELEMENTS --------------- //
+
+const login = document.querySelector(".login");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.getElementById("name");
+
+const chat = document.querySelector(".chat");
+const chatForm = document.querySelector(".chat-form");
+const chatInput = document.getElementById("name");
+
+// --------------- CONSTANTS --------------- //
 
 const user = {
-    id: "",
-    name: "",
-    color: "",
-    sessionId: ""
-}
+  id: "",
+  name: "",
+  color: ""
+};
 
+const randomColors = [
+  "cadetblue",
+  "darkgoldenrod",
+  "cornflowerblue",
+  "darkkhaki",
+  "hotpink",
+  "gold",
+];
 
-loginInput.addEventListener('submit', (event) => {
-    event.preventDefault();
-    user.name = loginInput.value;
-    event.value = '';
-    console.log(loginInput.value)
+let websocket;
+
+// --------------- EVENTS --------------- //
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const ramdomUser = `Usuário ${Math.ceil(Math.random() * 1000)}`;
+  const formatName = loginInput.value.trim();
+
+  user.name = formatName;
+  user.id = crypto.randomUUID();
+  user.color = getRandomColor();
+
+  login.style.display = 'none';
+  chat.style.display = 'flex';
+
+  websocket = new WebSocket('ws:localhost:8080');
+});
+
+chatForm.addEventListener("submit", (event) => {
+
 })
 
-console.log(user)
+// --------------- FUNCTIONS --------------- //
+
+function getRandomColor() {
+  const randomIndex = Math.floor(Math.random() * randomColors.length);
+  return randomColors[randomIndex];
+}
