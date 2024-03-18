8d8871fd7ddb34544b6776bc4131f4c03b078e2f
Denlavor <rafinhacrisdenlavor@gmail.com>
2024-03-18 01:40:58 -0300

    fix: animating home screen

--- frontend/js/utils/getRandomColor.js
+++ frontend/js/utils/getRandomColor.js
@@ -1,12 +1,3 @@
-const randomColors = [
-  "cadetblue",
-  "darkgoldenrod",
-  "cornflowerblue",
-  "darkkhaki",
-  "hotpink",
-  "gold",
-];
-
 function getRandomColor() {
   const randomIndex = Math.floor(Math.random() * randomColors.length);
   return randomColors[randomIndex];
